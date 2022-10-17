13a65892157fa5a8c5c3afbbf52a20a54b2fb572
Anthony Hansord <tonyhansorddev@gmail.com>
2022-10-17 20:17:52 +1100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const main = document.getElementById("main");
+main.remove();
+
+const body = document.querySelector("body");
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Tony is the champion";
+
+body.appendChild(newHeader);
 // Write your code here!
